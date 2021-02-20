fb91cc83fe76faa1883cac6cedc77ece6e3f8ba1
Mark Achée <mark@achee.com>
2021-02-20 12:21:47 -0600

    cargo fmt the hello example

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -2,18 +2,25 @@ use std::net::SocketAddr;
 
 use hyper::{Body, Method, Response};
 use macro_rules_attribute::macro_rules_attribute;
-use marla::{Request, serve, async_handler};
 use marla::config::{MarlaConfig, RegexPath, Route};
+use marla::{async_handler, serve, Request};
 use regex::Regex;
 
 #[tokio::main]
 async fn main() {
     let marla_config = MarlaConfig {
-        routers: vec![Box::new(vec![
-            RegexPath{ regex: Regex::new("^/hello/([a-zA-Z]{1,30})$").unwrap(), routes: vec![
-                (Method::GET, Route { handler: hello, middleware: None }),
-            ].into_iter().collect()},
-        ])],
+        routers: vec![Box::new(vec![RegexPath {
+            regex: Regex::new("^/hello/([a-zA-Z]{1,30})$").unwrap(),
+            routes: vec![(
+                Method::GET,
+                Route {
+                    handler: hello,
+                    middleware: None,
+                },
+            )]
+            .into_iter()
+            .collect(),
+        }])],
 
         middleware: vec![],
         listen_addr: SocketAddr::from(([127, 0, 0, 1], 3000)),
@@ -23,10 +30,6 @@ async fn main() {
 }
 
 #[macro_rules_attribute(async_handler!)]
-pub async fn hello(
-    request: Request,
-    _body: Option<Body>,
-    _bundle: (),
-) -> Response<Body> {
+pub async fn hello(request: Request, _body: Option<Body>, _bundle: ()) -> Response<Body> {
     Response::new(Body::from(format!("Hello, {}\n", request.path_params[0])))
 }
